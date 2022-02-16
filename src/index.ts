d5cb4da22ed772da7574f9ebd99fb4d4357ce285
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-16 20:58:39 -0300

    🔨 :hammer: [Fix] Server port from DB_PORT to PORT

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ app.use(cors());
 
 app.use('/user', userRouter);
 
-app.listen(process.env.DB_PORT || 3003, () => {
+app.listen(process.env.PORT || 3003, () => {
     console.log("Servidor rodando na porta 3003");
 });
